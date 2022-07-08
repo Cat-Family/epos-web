abec08080faf718e62d9e906ce10f0f862599fdf
cat <supercutcat@outlook.com>
2022-07-08 19:30:12 +0800

    change product ui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import SpeedDial from "@mui/material/SpeedDial";
@@ -9,29 +8,12 @@ import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
 import SaveIcon from "@mui/icons-material/Save";
 import PrintIcon from "@mui/icons-material/Print";
 import ShareIcon from "@mui/icons-material/Share";
-import { CardHeader, Container, Grid, IconButton, Stack } from "@mui/material";
+import { CardHeader, Container } from "@mui/material";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardMedia from "@mui/material/CardMedia";
-import Skeleton from "@mui/material/Skeleton";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
-import { styled } from "@mui/material/styles";
-import { Global } from "@emotion/react";
-import { grey } from "@mui/material/colors";
 import { useRecoilState } from "recoil";
 import menuState from "../state/menuState";
 import useMenuAction from "../actions/useMenuActions";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-
-const drawerBleeding = 84;
-
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
 
 const actions = [
   { icon: <FileCopyIcon />, name: "Copy" },
@@ -46,36 +28,10 @@ interface TabPanelProps {
   value: number;
 }
 
-const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "light" ? "#fff" : grey[800],
-}));
-
-const Puller = styled(Box)(({ theme }) => ({
-  width: 30,
-  height: 6,
-  backgroundColor: theme.palette.mode === "light" ? grey[300] : grey[900],
-  borderRadius: 3,
-  position: "absolute",
-  top: 8,
-  left: "calc(50% - 15px)",
-}));
-
-function a11yProps(index: number) {
-  return {
-    id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`,
-  };
-}
-
-export default function HomePage(props: Props) {
-  const { window } = props;
+export default function HomePage() {
   const menuAction = useMenuAction();
   const [menu, setMenu] = useRecoilState<any>(menuState);
   const [value, setValue] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
 
   useLayoutEffect(() => {
     menuAction.getMenu();
@@ -88,10 +44,6 @@ export default function HomePage(props: Props) {
     setValue(newValue);
   };
 
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen);
-  };
-
   return (
     <Box
       sx={{
@@ -100,17 +52,16 @@ export default function HomePage(props: Props) {
         width: 1,
       }}
     >
-      <Global
-        styles={{
-          ".MuiDrawer-root > .MuiPaper-root": {
-            height: `calc(50% - ${drawerBleeding}px)`,
-            overflow: "visible",
-          },
-        }}
-      />
-      <Box sx={{ width: "100vw", typography: "body1", display: "flex" }}>
+      <Box sx={{ width: "100vw", display: "flex" }}>
         <TabContext value={value}>
-          <Box sx={{ height: 1, position: "fixed" }}>
+          <Box
+            sx={{
+              height: 1,
+              position: "fixed",
+              borderRight: 1,
+              borderColor: "divider",
+            }}
+          >
             <TabList
               orientation="vertical"
               value={value}
@@ -129,10 +80,10 @@ export default function HomePage(props: Props) {
               ))}
             </TabList>
           </Box>
-          <Box sx={{ flexGrow: 1, pl: "4rem" }}>
+          <Box sx={{ flexGrow: 1, pl: "5rem" }}>
             {menu?.map((item: any) => (
-              <TabPanel value={item.categoryType}>
-                <Container
+              <TabPanel sx={{ pr: 0 }} value={item.categoryType}>
+                <Box
                   sx={{
                     display: "flex",
                     flexDirection: "row",
@@ -140,15 +91,15 @@ export default function HomePage(props: Props) {
                     gap: 1,
                   }}
                 >
-                  {item?.products.map((item: any, index: number) => (
-                    <Card sx={{ width: "8rem" }} key={index}>
+                  {item?.products.map((item: any) => (
+                    <Card sx={{ width: "5rem", pr: 0 }}>
                       <CardHeader
                         subheaderTypographyProps={{ fontSize: ".8em" }}
                         subheader={item.productName}
                       />
                     </Card>
                   ))}
-                </Container>
+                </Box>
               </TabPanel>
             ))}
           </Box>
