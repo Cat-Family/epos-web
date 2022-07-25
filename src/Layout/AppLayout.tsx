fad20a1fc06a628dd89a66091dd59b1bcba8179b
LiuLin <jiahaoprogrammer@outlook.com>
2022-07-25 23:47:59 +0800

    fix something

--- src/Layout/AppLayout.tsx
+++ src/Layout/AppLayout.tsx
@@ -1,84 +1,64 @@
-import React, { useLayoutEffect, useRef, useState } from "react";
+import React, {useLayoutEffect, useRef, useState} from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
-import { Outlet, useNavigate } from "react-router";
-import { Link } from "react-router-dom";
+import {Outlet, useNavigate} from "react-router";
+import AppBar from "@mui/material/AppBar/AppBar";
+import {Link} from "react-router-dom";
 import RoomServiceOutlinedIcon from "@mui/icons-material/RoomServiceOutlined";
 import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
 import DiningOutlined from "@mui/icons-material/DiningOutlined";
 import RestoreIcon from "@mui/icons-material/Restore";
-import { useLocation } from "react-router-dom";
-import type { DrawerProps, RadioChangeEvent } from "antd";
-import * as antd from "antd";
+import {useLocation} from "react-router-dom";
+import type {DrawerProps, RadioChangeEvent} from 'antd';
 import useLoginOutActions from "../actions/useUserActions";
 import useTableAction from "../actions/useTableActions";
 import {
-  ArrowForwardIosOutlined,
-  Brightness4Outlined,
-  Brightness7Outlined,
-  GTranslateOutlined,
-  Logout,
-  Settings,
-  SwapHorizOutlined,
+    ArrowForwardIosOutlined,
+    Brightness7Outlined,
+    GTranslateOutlined,
+    Logout,
+    Settings,
 } from "@mui/icons-material";
-import { Avatar, MenuItem, Menu, ListItemIcon } from "@mui/material";
-import { useRecoilState } from "recoil";
-import menuState from "../state/menuState";
+import {
+    IconButton,
+    Toolbar,
+    Typography,
+    Avatar,
+    MenuItem,
+    Menu,
+    ListItemIcon,
+    Divider,
+    Button,
+} from "@mui/material";
+import {useRecoilState} from "recoil";
 import tableState from "../state/tableState";
-import { TabPanel } from "@mui/lab";
-import CloseIcon from "@mui/icons-material/Close";
-import Slide from "@mui/material/Slide";
-import { TransitionProps } from "@mui/material/transitions";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import ListItemText from "@mui/material/ListItemText";
-import ListItem from "@mui/material/ListItem";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & {
-    children: React.ReactElement;
-  },
-  ref: React.Ref<unknown>
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
+import TableDrawer from "../components/TableDrawer";
 
 const AppLayout = () => {
-  const profileMenuId = "primary-account-menu";
-  const location = useLocation();
-  const loginOutActions = useLoginOutActions();
-  const tableAction = useTableAction();
-  const [anchorProfileMenu, setAnchorProfileMenu] =
-    React.useState<null | HTMLElement>(null);
-  const isProfileMenuOpen = Boolean(anchorProfileMenu);
-  const [visible, setVisible] = useState(false);
-  const [disable, setDisable] = useState(false);
-  const [table, setTable] = useRecoilState<any>(tableState);
-  const [placement, setPlacement] = useState<DrawerProps["placement"]>("top");
+    const profileMenuId = "primary-account-menu";
+    const location = useLocation();
+    const loginOutActions = useLoginOutActions();
+    const tableAction = useTableAction();
+    const openTableStage = useRef<any>();
+    const tableDrawer = useRef<any>();
+    const [anchorProfileMenu, setAnchorProfileMenu] =
+        React.useState<null | HTMLElement>(null);
+    const isProfileMenuOpen = Boolean(anchorProfileMenu);
+    const [visible, setVisible] = useState(false);
+    const [table, setTable] = useRecoilState<any>(tableState);
+    const [placement, setPlacement] = useState<DrawerProps['placement']>('top');
 
-  const onClose = () => {
-    setVisible(false);
-  };
+    const logout = async () => {
+        await loginOutActions.loginOut();
+    };
 
-  const onChange = (e: RadioChangeEvent) => {
-    setPlacement(e.target.value);
-  };
+    const queryTable = async () => {
+        await tableAction.getTables();
+    }
 
-  const logout = async () => {
-    await loginOutActions.loginOut();
-  };
 
-  const queryTable = async () => {
-    const a = await tableAction.getTables();
-  };
     useLayoutEffect(() => {
         tableAction.getTables();
     }, []);
@@ -108,56 +88,48 @@ const AppLayout = () => {
     );
     const ref = React.useRef<HTMLDivElement>(null);
     const navigate = useNavigate();
-  const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const renderProfileMenu = (
+        <Menu
+            anchorEl={anchorProfileMenu}
+            id={profileMenuId}
+            keepMounted
+            open={isProfileMenuOpen}
+            onClose={handleProfileMenuClose}
+            PaperProps={{
+                elevation: 0,
+                sx: {
+                    width: 250,
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                    },
+                    "&:before": {
+                        content: '""',
+                        display: "block",
+                        position: "absolute",
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
+                        zIndex: 0,
+                    },
+                },
+            }}
+            transformOrigin={{horizontal: "right", vertical: "top"}}
+            anchorOrigin={{horizontal: "right", vertical: "bottom"}}
+        >
+            <MenuItem>
+                <Avatar src="https://mui.com/static/images/avatar/1.jpg"></Avatar>
+            </MenuItem>
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const renderProfileMenu = (
-    <Menu
-      anchorEl={anchorProfileMenu}
-      id={profileMenuId}
-      keepMounted
-      open={isProfileMenuOpen}
-      onClose={handleProfileMenuClose}
-      PaperProps={{
-        elevation: 0,
-        sx: {
-          width: 250,
-          overflow: "visible",
-          filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-          mt: 1.5,
-          "& .MuiAvatar-root": {
-            width: 32,
-            height: 32,
-            ml: -0.5,
-            mr: 1,
-          },
-          "&:before": {
-            content: '""',
-            display: "block",
-            position: "absolute",
-            top: 0,
-            right: 14,
-            width: 10,
-            height: 10,
-            bgcolor: "background.paper",
-            transform: "translateY(-50%) rotate(45deg)",
-            zIndex: 0,
-          },
-        },
-      }}
-      transformOrigin={{ horizontal: "right", vertical: "top" }}
-      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-    >
-      <MenuItem>
-        <Avatar src="https://mui.com/static/images/avatar/1.jpg"></Avatar>
-      </MenuItem>
             <MenuItem
                 onClick={handleProfileMenuClose}
                 component={Link}
@@ -192,57 +164,56 @@ const AppLayout = () => {
                     {/* <Brightness4Outlined fontSize="small" /> */}
                 </ListItemIcon>
                 <Typography sx={{flexGrow: "1"}}>Appearance</Typography>
-        <ListItemIcon sx={{ justifyContent: "end" }}>
-          <ArrowForwardIosOutlined fontSize="small" />
-        </ListItemIcon>
-      </MenuItem>
-      <MenuItem
-        sx={{ display: "flex" }}
-        onClick={() => {
-          handleProfileMenuClose();
-        }}
-      >
-        <ListItemIcon>
-          <GTranslateOutlined />
-        </ListItemIcon>
-        <Typography sx={{ flexGrow: "1" }}>Language</Typography>
-        <ListItemIcon sx={{ justifyContent: "end" }}>
-          <ArrowForwardIosOutlined fontSize="small" />
-        </ListItemIcon>
-      </MenuItem>
-    </Menu>
-  );
-  return (
-    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Link to="/">
+
+                <ListItemIcon sx={{justifyContent: "end"}}>
+                    <ArrowForwardIosOutlined fontSize="small"/>
+                </ListItemIcon>
+            </MenuItem>
+            <MenuItem
+                sx={{display: "flex"}}
+                onClick={() => {
+                    handleProfileMenuClose();
+                }}
+            >
+                <ListItemIcon>
+                    <GTranslateOutlined/>
+                </ListItemIcon>
+                <Typography sx={{flexGrow: "1"}}>Language</Typography>
+                <ListItemIcon sx={{justifyContent: "end"}}>
+                    <ArrowForwardIosOutlined fontSize="small"/>
+                </ListItemIcon>
+            </MenuItem>
+        </Menu>
+    );
+    return (
+        <Box sx={{display: "flex", flexDirection: "column", height: "100vh"}}>
+            <AppBar position="fixed">
+                <Toolbar>
+                    {/* <Link to="/">
             <img
               className="logo"
               style={{ height: 32, width: 32 }}
               src="https://qy-jz.oss-cn-beijing.aliyuncs.com/jz/%E9%AB%98%E6%B8%85logo%E9%80%8F%E6%98%8E%E5%BA%95%E5%8E%9F%E8%89%B2%E5%AD%97.png"
               alt="logo"
             />
-          </Link>
+          </Link> */}
+
 
                     <Box sx={{flexGrow: 1}}/>
-          {/*TODO*/}
-          {/*这里的1号，需要展示为用户开台成功之后的桌号或者点击已经开台桌号*/}
-          {location.pathname === "/" && (
-            <>
-              <Button
-                sx={{ fontSize: "1.2rem" }}
-                color="inherit"
-                onClick={handleClickOpen}
-              >
-                1号
-              </Button>
-              <IconButton color="inherit" onClick={showDrawer}>
-                <SwapHorizOutlined fontSize="small" />
-              </IconButton>
-              <Box sx={{ flexGrow: 1 }} />
-            </>
-          )}
+
+                    {/*TODO*/}
+                    {/*这里的1号，需要展示为用户开台成功之后的桌号或者点击已经开台桌号*/}
+                    {location.pathname === "/" && (
+                        <>
+                            <Button sx={{fontSize: "1.2rem"}} color="inherit"  onClick={() => {
+                                tableDrawer.current.tableDrawerOpen();
+                            }}>
+                                1号
+                            </Button>
+
+                            <Box sx={{flexGrow: 1}}/>
+                        </>
+                    )}
 
                     <IconButton
                         size="large"
@@ -277,101 +248,42 @@ const AppLayout = () => {
             >
             </div>
             <BottomNavigation/>
-      <antd.Drawer
-        title="桌号选择"
-        placement={placement}
-        closable={false}
-        onClose={onClose}
-        visible={visible}
-        key={placement}
-        height="200"
-      >
-        {/*<Box sx={{display: "flex"}}>*/}
-        <antd.Space size={[18, 22]} wrap>
-          {table?.map((item: any) => (
-            <antd.Button type="primary" size="large" shape="circle">
-              {item.tableNum}
-            </antd.Button>
-            // <Button sx={{ width: "0.5rem", height: "2rem" }} color="info" variant="outlined">{item.tableNum}</Button>
-          ))}
-        </antd.Space>
 
-        {/*</Box>*/}
-      </antd.Drawer>
-
-      <BottomNavigation />
-
-      <Paper
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-            navigate(
-              newValue === 0
-                ? "/"
-                : newValue === 1
-                ? "/orders"
-                : newValue === 2
-                ? "/bills"
-                : "/report"
-            );
-          }}
-        >
-          <BottomNavigationAction label="点餐" icon={<DiningOutlined />} />
-          <BottomNavigationAction
-            label="订单"
-            icon={<RoomServiceOutlinedIcon />}
-          />
-          <BottomNavigationAction label="账单" icon={<RestoreIcon />} />
-          <BottomNavigationAction
-            label="报表"
-            icon={<AssignmentOutlinedIcon />}
-          />
-        </BottomNavigation>
-      </Paper>
-      <Dialog
-        fullScreen
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Transition}
-      >
-        <AppBar sx={{ position: "relative" }}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="close"
+            <Paper
+                sx={{position: "fixed", bottom: 0, left: 0, right: 0}}
+                elevation={3}
             >
-              <CloseIcon />
-            </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              Sound
-            </Typography>
-            <Button autoFocus color="inherit" onClick={handleClose}>
-              save
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <List>
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
-          </ListItem>
-        </List>
-      </Dialog>
-    </Box>
-  );
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                        navigate(
+                            newValue === 0
+                                ? "/"
+                                : newValue === 1
+                                ? "/orders"
+                                : newValue === 2
+                                    ? "/bills"
+                                    : "/report"
+                        );
+                    }}
+                >
+                    <BottomNavigationAction label="点餐" icon={<DiningOutlined/>}/>
+                    <BottomNavigationAction
+                        label="订单"
+                        icon={<RoomServiceOutlinedIcon/>}
+                    />
+                    <BottomNavigationAction label="账单" icon={<RestoreIcon/>}/>
+                    <BottomNavigationAction
+                        label="报表"
+                        icon={<AssignmentOutlinedIcon/>}
+                    />
+                </BottomNavigation>
+            </Paper>
+            <TableDrawer ref={tableDrawer}/>
+        </Box>
+    );
 };
 
 export default AppLayout;
